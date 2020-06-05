85994d1468f17f791305df01d0c46edbeaa01de9
Philipp <41761223+philipp2310@users.noreply.github.com>
2020-06-06 01:10:29 +0200

    workingcopy 0.8 moved tasmota, reworked voice addition of devices

---
+++ device/device_espSwitch.py
@@ -0,0 +1,51 @@
+from core.device.model.Device import Device
+from core.device.model.Location import Location
+from core.device.model.DeviceType import DeviceType
+from core.commons import constants
+import sqlite3
+import threading
+import socket
+from core.base.model.ProjectAliceObject import ProjectAliceObject
+from core.dialog.model.DialogSession import DialogSession
+from skills.Tasmota import Tasmota
+from core.device.model.DeviceException import requiresWIFISettings
+
+
+class device_espSwitch(DeviceType):
+
+	DEV_SETTINGS = ""
+	LOC_SETTINGS = ""
+	ESPTYPE = "switch"
+
+	def __init__(self, data: sqlite3.Row):
+		super().__init__(data, self.DEV_SETTINGS, self.LOC_SETTINGS)
+
+
+	def discover(self, device: Device, uid: str, replyOnSiteId: str = "", session:DialogSession = None) -> bool:
+		if not self.getAliceConfig('ssid'):
+			raise requiresWIFISettings()
+
+		self.parentSkillInstance.startTasmotaFlashingProcess(device, uid, replyOnSiteId, session)
+		pass
+
+
+	def getDeviceIcon(self, device: Device) -> str:
+		if not device.uid:
+			return 'device_espSwitch.png'
+		if not device.connected:
+			return 'switch_offline.png'
+
+		if device.getCustomValue('on'):
+			return 'switch_on.png'
+		else:
+			return 'switch_off.png'
+
+
+	def getDeviceConfig(self):
+		# return the custom configuration of that deviceType
+		pass
+
+
+	def toggle(self, device: Device):
+		# todo toggle switch?
+		pass
